605eecce152c8705355f1d67531891f63515eb1e
Thai Nguyen <36321686+thaithichg123@users.noreply.github.com>
2021-05-17 15:18:07 -0500

    Update CURL.h

--- CURL.h
+++ CURL.h
@@ -5,7 +5,7 @@ void runCurl() {
   // curl is command line program for transferring data using different internet protocols
   Process p;    // Create a process and call it "p"
   p.begin("curl");  // Process that launch the "curl" command
-  p.addParameter("https://maker.ifttt.com/trigger/DOOR/with/key/cB_0zhBl4fJ3EOqJ1VQ9L7"); // Add the URL parameter to "curl"
+  p.addParameter("Put in the link you want to curl"); // Add the URL parameter to "curl"
   p.run();    // Run the process and wait for its termination
 
 
